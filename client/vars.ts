203c23c1789bd937716464a49b99d1bfb7ca1c62
vitalik-fan <63237530+vitalik-fan@users.noreply.github.com>
2021-10-23 12:55:02 +0300

    начал переделывать модель пользователя

--- client/vars.ts
+++ client/vars.ts
@@ -1,1 +1,2 @@
 export const back_url = "http://localhost:1337"
+export const secret_key = "andreas-secret-key-for-passw"
--- server/api/user/controllers/users.js
+++ server/api/user/controllers/users.js
@@ -4,5 +4,12 @@ const loggedService = require('../services/users')
 module.exports = {
   async logged(ctx) {
     return loggedService.logged(ctx);
+  },
+  async change(ctx) {
+    try {
+      console.log(ctx.state.user)
+    } catch (e) {
+      return e.message;
+    }
   }
 };
