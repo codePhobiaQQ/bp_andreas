0e6fecf601276b51b4a5a576ed9a9c0309254955
vitalik-fan <63237530+vitalik-fan@users.noreply.github.com>
2021-12-14 19:18:47 +0300

    go to imac

--- client/components/sections/TeachersSection.tsx
+++ client/components/sections/TeachersSection.tsx
@@ -3,7 +3,7 @@ import { useInView } from "react-intersection-observer";
 import LeftRightVariants from "../../variants/LeftRightVariants";
 import { Container } from "reactstrap"
 import andreasPhoto from "../../public/assets/img/auth-img.png";
-import { Swiper, SwiperSlide } from 'swiper/react';
+// import { Swiper, SwiperSlide } from 'swiper/react';
 
 export default function TeachersSection() {
     const { ref, inView } = useInView({
@@ -29,20 +29,20 @@ export default function TeachersSection() {
             <span>You know these guys</span>
             <h2>Featured teachers</h2>
             <Container>
-            <Swiper
-                className="teachersSlider"
-                slidesPerView={1}
-            >
-                <SwiperSlide>
-                    <img src={andreasPhoto.src} alt="teacher" />
-                </SwiperSlide>
-                <SwiperSlide>
-                    <img src={andreasPhoto.src} alt="teacher" />
-                </SwiperSlide>
-                <SwiperSlide>
-                    <img src={andreasPhoto.src} alt="teacher" />
-                </SwiperSlide>
-            </Swiper>
+            {/*<Swiper*/}
+            {/*    className="teachersSlider"*/}
+            {/*    slidesPerView={1}*/}
+            {/*>*/}
+            {/*    <SwiperSlide>*/}
+            {/*        <img src={andreasPhoto.src} alt="teacher" />*/}
+            {/*    </SwiperSlide>*/}
+            {/*    <SwiperSlide>*/}
+            {/*        <img src={andreasPhoto.src} alt="teacher" />*/}
+            {/*    </SwiperSlide>*/}
+            {/*    <SwiperSlide>*/}
+            {/*        <img src={andreasPhoto.src} alt="teacher" />*/}
+            {/*    </SwiperSlide>*/}
+            {/*</Swiper>*/}
             </Container>
         </motion.section>
     )
